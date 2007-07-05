23baef14d716756063c6fb8aa2906e9e4d62ae40
Uwe Kubosch <uwe@kubosch.no>
2007-07-05 20:44:57 +0000

    * fikset deployment script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,10 @@
 set :application, "front"
 set :repository, "svn+ssh://donv@source.kubosch.no/var/svn/trunk/#{application}"
 
-#role :web, "www.kubosch.no"
 role :app, "www.kubosch.no"
 role :db,  "www.kubosch.no", :primary => true
 
-# set :deploy_to, "/path/to/app" # defaults to "/u/apps/#{application}"
-set :user, "donv"                # defaults to the currently logged in user
+set :user, "donv"
 set :use_sudo, false
 
 desc "The spinner task is used by :cold_deploy to start the application up"
