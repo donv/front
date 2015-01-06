d2805794c8a2d230d27f7f998571dd4ea5e634be
Uwe Kubosch <uwe@kubosch.no>
2015-01-06 14:40:32 +0000

    * Cleanup + fix textile.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def txt(textile_source)
+    textilize(textile_source).html_safe
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# create_table "news_items", force: true do |t|
+#   t.string   "title",      limit: 64, null: false
+#   t.text     "body",                  null: false
+#   t.datetime "created_at"
+#   t.datetime "updated_at"
+# end
+
+Site.create! title: 'Main site', welcome_text: "Welcome\n* One\n* Two"
+
+# create_table "users", force: true do |t|
+#   t.string   "login"
+#   t.string   "email"
+#   t.string   "crypted_password",          limit: 40
+#   t.string   "salt",                      limit: 40
+#   t.string   "remember_token"
+#   t.datetime "remember_token_expires_at"
+#   t.datetime "created_at"
+#   t.datetime "updated_at"
+# end
