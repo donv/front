efdb0d7dc73b15ae9ce5d4b08cb9e94c8547d346
Uwe Kubosch <uwe@datek.no>
2019-03-27 19:32:21 +0100

    Update to Rails 5.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,11 @@
 Rails.application.configure do
+  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  # config.action_cable.mount_path = nil
+  # config.action_cable.url = 'wss://example.com/cable'
   # config.action_controller.asset_host = 'http://assets.example.com'
   config.action_controller.perform_caching = true
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
   config.action_mailer.perform_caching = false
   # config.action_mailer.raise_delivery_errors = false
   # config.active_job.queue_adapter     = :resque
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.log_tags = [:request_id]
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+  config.read_encrypted_secrets = true
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
