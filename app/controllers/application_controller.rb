681955cd94beb800711d605fc9d1bb5013d60005
Uwe Kubosch <uwe@datek.no>
2020-10-29 13:27:50 +0100

    Remove links to old sites

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,10 +25,6 @@ class ApplicationController < ActionController::Base
       { title: t(:sections),
         content: <<~HTML
           <ul>
-            <li><a href="~cuddles">Lise's photos</a></li>
-            <li><a href="~donv">Uwe's Home Page</a></li>
-                      <li><a href="http://shop.kubosch.no/">The Shop</a></li>
-            <li><a href="http://backlog.kubosch.no/">The Backlog</a></li>
             <li><a href="http://blog.kubosch.no/">The Blog</a></li>
           </ul>
         HTML
